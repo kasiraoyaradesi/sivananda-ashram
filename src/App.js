cc34823a5210cf966f2412f3715dd6245f28dc4b
kasiraoyaradesi <you@example.com>
2024-10-01 15:02:23 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
+import Navbar from './Components/navbar/Navbar';
+import Home from './Components/slider/Home';
+import Footer from './Components/footer/Footer';
+import Ashram from './Components/home/Ashram';
+import AboutAshram from './Components/home/AboutAshram';
+import AshramServices from './Components/home/AshramServices';
+import Photo from './Components/photogallery/Photo';
+import Scriptures from './Components/scriptures/Scriptures';
+import Contact from './Components/contact/Contact';
+import Yogaday from './Components/contact/Yogaday'
+import Videos from './Components/videos/Videos';
+import News from './Components/news/News';
+import KnowMore from './Components/slider/KnowMore'; // Import the KnowMore component
+import Header from './Components/header/Header';
+import SwamiSivananda from './Components/slider/SwamiSivananda'; // Import the new page component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Header />
+      <Routes >
+        <Route path="/" element={
+            <>
+              <Home />
+              <AboutAshram />  {/* These will now only be visible on the home page */}
+              <AshramServices />
+            </>
+          } 
+        />
+        <Route path="/ashram" element={<Ashram />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/photogallery" element={<Photo />} />
+        <Route path="/scriptures" element={<Scriptures />} />
+        <Route path='/Yogaday' element={<Yogaday/>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/videos" element={<Videos />} /> 
+        <Route path="/knowmore" element={<KnowMore />} />
+        <Route path="/swami-sivananda" element={<SwamiSivananda />} />
+
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
